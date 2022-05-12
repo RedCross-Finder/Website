acb09653092638f187b1c11db757b1afb9c474ed
Nick van Lierop <nicklierop@gmail.com>
2022-05-12 20:45:58 +0200

    initial commit to production, created first app with just title text

--- redCrossFinder/redCrossFinder/urls.py
+++ redCrossFinder/redCrossFinder/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from homescreen.views import home
+
 urlpatterns = [
+    path('', home, name="homepage"),
     path('admin/', admin.site.urls),
 ]
