c6d5c3f4bee96b004901733c172d0dfd1b0fce78
1999nick1999 <nickvanlierop.design@gmail.com>
2022-05-17 21:50:13 +0200

    Created sign up page and added first style with css and favicon

--- redCrossFinder/homescreen/views.py
+++ redCrossFinder/homescreen/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+#from django.shortcuts import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def home (request):
-    return HttpResponse('<h1>Hello world!</h1>')
+    return render(request, 'home.html', {})
+
+def login (request):
+    return render(request, 'login.html', {})
+
+def register (request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
